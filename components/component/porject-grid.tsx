8b413c0f9c667817d0f2f8114ddbe0b8308af315
Mohd Kaif <kaifmohd2014@gmail.com>
2024-07-04 14:53:08 +0530

    chore: Update project grid to include links to repository pages

--- components/component/porject-grid.tsx
+++ components/component/porject-grid.tsx
@@ -5,6 +5,7 @@ import { GitHubLogoIcon } from '@radix-ui/react-icons'
 import { cn } from '@/lib/utils'
 import { SparklesIcon } from '../ui/icons'
 import { useUIState, useActions } from 'ai/rsc'
+import Link from 'next/link'
 
 export function ProjectGrid() {
   // fetch most starred repositories of kaifcoder in descending order
@@ -35,58 +36,40 @@ export function ProjectGrid() {
           items.map(
             (
               item: {
-                full_name: ReactNode
-                name:
-                  | string
-                  | number
-                  | bigint
-                  | boolean
-                  | React.ReactElement<
-                      any,
-                      string | React.JSXElementConstructor<any>
-                    >
-                  | Iterable<React.ReactNode>
-                  | React.ReactPortal
-                  | Promise<React.AwaitedReactNode>
-                  | null
-                  | undefined
-                description:
-                  | string
-                  | number
-                  | bigint
-                  | boolean
-                  | React.ReactElement<
-                      any,
-                      string | React.JSXElementConstructor<any>
-                    >
-                  | Iterable<React.ReactNode>
-                  | React.ReactPortal
-                  | Promise<React.AwaitedReactNode>
-                  | null
-                  | undefined
+                [x: string]: URL
+                homepage: any
+                full_name: any
+                name: any
+                description: any
               },
               i: React.Key | null | undefined
             ) => (
-              <BentoGridItem
-                key={i}
-                title={item.name}
-                description={
-                  <div className="h-full flex flex-col">
-                    <p className="text-muted-foreground text-xs flex-1">
-                      {item.description}
-                    </p>
-                    <div className="flex mt-5 items-center gap-2 text-muted-foreground text-sm">
-                      <GitHubLogoIcon className="size-5" />
-                      <span>{item.full_name}</span>
+              // eslint-disable-next-line react/jsx-key
+              <>
+                <BentoGridItem
+                  key={i}
+                  title={item.name}
+                  description={
+                    <div className="h-full flex flex-col">
+                      <p className="text-muted-foreground text-xs flex-1">
+                        {item.description}
+                      </p>
+                      <div className="flex mt-5 items-center gap-2 text-muted-foreground text-sm">
+                        <GitHubLogoIcon className="size-5" />
+                        <span>{item.full_name}</span>
+                      </div>
                     </div>
-                  </div>
-                }
-                icon={<GitHubLogoIcon className="size-4 text-neutral-500" />}
-                className={cn(
-                  i === 3 || i === 6 ? 'md:col-span-2' : '',
-                  'bg-gray-100 cursor-pointer hover:shadow-lg p-6 rounded-xl transition-colors'
-                )}
-              />
+                  }
+                  icon={<GitHubLogoIcon className="size-4 text-neutral-500" />}
+                  className={cn(
+                    i === 3 || i === 6 ? 'md:col-span-2' : '',
+                    'bg-gray-100 cursor-pointer hover:shadow-lg p-6 rounded-xl transition-colors'
+                  )}
+                  onClick={() => {
+                    window.open(item.html_url, '_blank')
+                  }}
+                />
+              </>
             )
           )}
       </BentoGrid>
