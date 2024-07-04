61309a0f28e8c4e31cbb3f66e47123bdd1caa8b5
Mohd Kaif <kaifmohd2014@gmail.com>
2024-07-04 14:59:50 +0530

    chore: Update max-w-2xl to max-w-3xl in EmptyScreen component

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -3,11 +3,11 @@ import { LinkPreview } from './ui/link-preview'
 
 export function EmptyScreen() {
   return (
-    <div className="mx-auto max-w-2xl px-4">
+    <div className="mx-auto max-w-3xl px-4">
       <div className="flex flex-col gap-2 rounded-2xl bg-zinc-50 sm:p-8 p-4 text-sm sm:text-base">
         <h1 className="text-2xl sm:text-3xl tracking-tight font-semibold max-w-fit inline-block">
           {/* Generative ui portfolio */}
-          Generative UI Portfolio
+          AI Portfolio of Mohd Kaif
         </h1>
         <p className="leading-normal text-zinc-900">
           {/* describe this portfolio to users */}
