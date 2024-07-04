5faf0d37aa2a5a11cf8f2825f70c8870872e72ab
Mohd Kaif <kaifmohd2014@gmail.com>
2024-07-04 16:58:41 +0530

    chore: Update GitHub repository link in header component

--- components/header.tsx
+++ components/header.tsx
@@ -53,7 +53,7 @@ export function Header() {
         <Button asChild size="sm" variant="ghost">
           <a
             target="_blank"
-            href="https://github.com/vercel-labs/gemini-chatbot"
+            href="https://github.com/kaifcoder/gemini-ai-portfolio"
             rel="noopener noreferrer"
           >
             <IconGitHub />
